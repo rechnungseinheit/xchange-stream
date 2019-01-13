d311be1c5a37a301718c960dc887c38f9fa5e9d8
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-01-13 14:02:46 +0000

    Update release script

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn release:prepare --batch-mode -DreleaseVersion=4.3.11.orko.6 -DdevelopmentVersion=4.3.14.orko.1-SNAPSHOT -Dresume=false -DpushChanges=false
+mvn release:prepare --batch-mode -DreleaseVersion=4.3.14.orko.1 -DdevelopmentVersion=4.3.14.orko.2-SNAPSHOT -Dresume=false -DpushChanges=false
